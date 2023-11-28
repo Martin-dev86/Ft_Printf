b19b05188efa7360dece13aebe6010ca426b86f5
Carlos Garcia Martin <cagarci2@c3r4s5.42malaga.com>
2023-11-28 12:10:06 +0100

    añadida nuevas funciones

--- sources/printf.c
+++ sources/printf.c
@@ -6,12 +6,36 @@
 /*   By: cagarci2 <cagarci2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/28 10:34:39 by cagarci2          #+#    #+#             */
-/*   Updated: 2023/11/28 10:59:42 by cagarci2         ###   ########.fr       */
+/*   Updated: 2023/11/28 12:09:42 by cagarci2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../library/printf.h"
 #include "../library/libft.h"
+
+int	print_format(char simbol, va_list list)
+{
+	int	count;
+
+	count = 0;
+	if (simbol == 'c')
+		count += ft_putchar(va_arg(ap, int));
+	else if (simbol == 's')
+		count += ft_putstr(va_arg(ap, char *));
+	else if (simbol == 'p')
+		count += ft_putvoid(va_arg(ap, *void));
+	else if (simbol == 'd' || simbol == 'i')
+		count += ft_putdigit(va_arg(ap, int), 10);
+	else if (simbol == 'u')
+		count += ft_putdigit(va_arg(ap, unsigned int), 10);
+	else if (simbol == 'x')
+		count += ft_puthex_lower(va_arg(ap, unsigned int), 16);
+	else if (simbol == 'X')
+		count += ft_puthex_upper(va_arg(ap, unsigned int), 16);
+	else
+		write(1, &simbol, 1);
+	return (count);
+}
 
 int	printf(const char *format, ...)
 {
@@ -26,6 +50,7 @@ int	printf(const char *format, ...)
 			count += print_format(*(++format), ap);
 		else
 			count += write(1, format, 1);
+		++format;
 	}
 	va_end(ap);
 	return (count);
